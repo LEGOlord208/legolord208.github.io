a1cf0a4e5aac902dbbce438a7f141535f8005347
jD91mZM2 <me@krake.one>
2017-10-03 16:20:03 +0200

    Space > tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,47 @@
 $(document).ready(function(){
     let win = $(window);
-	let menu = $("#menu");
+    let menu = $("#menu");
 
-	win.scroll(function(){
-		menu.toggleClass("hide", win.scrollTop() + win.height() > $(document).height() - 100);
-	});
+    win.scroll(function(){
+        menu.toggleClass("hide", win.scrollTop() + win.height() > $(document).height() - 100);
+    });
 
-	let imgzoom = $("#imgzoom");
+    let imgzoom = $("#imgzoom");
 
-	$("img").click(function(e){
-		e.stopPropagation();
+    $("img").click(function(e){
+        e.stopPropagation();
 
-		let imgzoomimg = $("img", imgzoom);
-		imgzoomimg.prop("src", $(this).prop("src"));
-		imgzoom.addClass("visible");
-	});
+        let imgzoomimg = $("img", imgzoom);
+        imgzoomimg.prop("src", $(this).prop("src"));
+        imgzoom.addClass("visible");
+    });
 
-	$(this).click(function(){
-		imgzoom.removeClass("visible");
-	});
+    $(this).click(function(){
+        imgzoom.removeClass("visible");
+    });
 
-	if(document.cookie.indexOf("cookies=") < 0) {
-		setTimeout(function(){
-			$("#cookies, #dim").addClass("visible").trigger("visible");
-		}, 5000);
+    if(document.cookie.indexOf("cookies=") < 0) {
+        setTimeout(function(){
+            $("#cookies, #dim").addClass("visible").trigger("visible");
+        }, 5000);
 
-		$("#cookies button").click(function(){
-			$("#cookies, #dim").removeClass("visible");
+        $("#cookies button").click(function(){
+            $("#cookies, #dim").removeClass("visible");
 
-			let d = new Date();
-			d.setTime(d.getTime() + 1000 * 60 * 60 * 24 * 365 * 50);
-			document.cookie = "cookies=on; expires=" + d.toUTCString() + "; path=/";
-		});
-	}
+            let d = new Date();
+            d.setTime(d.getTime() + 1000 * 60 * 60 * 24 * 365 * 50);
+            document.cookie = "cookies=on; expires=" + d.toUTCString() + "; path=/";
+        });
+    }
 
-	$("#dim").on("transitionend", function(){
-		let me = $(this);
+    $("#dim").on("transitionend", function(){
+        let me = $(this);
 
-		if(!me.is(".visible"))
-			me.hide();
-	}).on("visible", function(){
-		let me = $(this);
-		me.show();
-	});
-	console.log("%c¯\\_(ツ)_/¯", "color: green;font-size: 40px;");
+        if(!me.is(".visible"))
+            me.hide();
+    }).on("visible", function(){
+        let me = $(this);
+        me.show();
+    });
+    console.log("%c¯\\_(ツ)_/¯", "color: green;font-size: 40px;");
 });
--- assets/js/tir.js
+++ assets/js/tir.js
@@ -1,36 +1,36 @@
 $(window).ready(function(){
-	$("#suggest").submit(function(){
-		let link =
-			"https://twitter.com/intent/tweet?text="
-			+ "@LEGOlord208! I have "
-			+ $("#suggest-type").find(":checked").data("label")
-			+ " the TIR "
-			+ $("#suggest-regarding").val() + ": ";
-		location.href = link;
+    $("#suggest").submit(function(){
+        let link =
+            "https://twitter.com/intent/tweet?text="
+            + "@LEGOlord208! I have "
+            + $("#suggest-type").find(":checked").data("label")
+            + " the TIR "
+            + $("#suggest-regarding").val() + ": ";
+        location.href = link;
 
-		return false;
-	});
-	$("#download-form").submit(function(){
-		location.href = $(this).find("select :checked").data("link");
-		return false;
-	}).find("select").change(function(){
-		let me = $(this);
-		let p = $("#virusWarning32");
-		p.toggle(me.find(":checked").text() === "Windows (x32)");
-	});
-	$("#about-toggle").click(function(){
-		let about = $("#about").show();
-		setTimeout(function(){
-			about.toggleClass("visible");
-		}, 100);
-	});
-	$("#about").on("transitionend", function(){
-		let me = $(this);
-		if(!me.is(".visible"))
-			$(this).hide();
-	});
+        return false;
+    });
+    $("#download-form").submit(function(){
+        location.href = $(this).find("select :checked").data("link");
+        return false;
+    }).find("select").change(function(){
+        let me = $(this);
+        let p = $("#virusWarning32");
+        p.toggle(me.find(":checked").text() === "Windows (x32)");
+    });
+    $("#about-toggle").click(function(){
+        let about = $("#about").show();
+        setTimeout(function(){
+            about.toggleClass("visible");
+        }, 100);
+    });
+    $("#about").on("transitionend", function(){
+        let me = $(this);
+        if(!me.is(".visible"))
+            $(this).hide();
+    });
 });
 
 $.getJSON("/files/tir-titles.json", function(titles){
-	document.title = "TIR - " + titles[Math.floor(Math.random() * titles.length)];
+    document.title = "TIR - " + titles[Math.floor(Math.random() * titles.length)];
 });
